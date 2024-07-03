c8efb481ff3cf35dfe02b233cf8ea9e9f4b1ac51
mahalakshmibyreddy <byreddymahalakshmi26@gmail.com>
2024-07-03 13:46:38 +0530

    added new project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      grabcursor:true,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  
+
+
